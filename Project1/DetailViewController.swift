9e34296cb0111ab62f24fc51ffdb5cb9fbb3b16b
kabikira <kabikirar2@protonmail.com>
2023-01-15 18:10:39 +0900

    add day22 challenge

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -38,6 +38,9 @@ class DetailViewController: UIViewController {
         guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
             print("No image found")
             return
+//        guard let selectedImage = selectedImage else {
+//            print("No name")
+//            return
         }
 
         let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
