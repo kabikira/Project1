47acca8c31b722d81d4c13baaf843c4181bb0733
kabikira <kabikirar2@protonmail.com>
2023-01-15 12:54:24 +0900

    add day22

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -19,7 +19,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         title = "Picture \(selectedPictureNumber) of \(totalPictures)"
-        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image  = UIImage(named: imageToLoad)
@@ -33,6 +33,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     
